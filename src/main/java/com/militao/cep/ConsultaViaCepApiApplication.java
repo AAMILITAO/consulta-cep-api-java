956a490b65453d1d19220d86fc705a512b63d71d
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2020-02-24 23:04:27 -0300

    Criaçao de api para consumir ceps

---
+++ src/main/java/com/militao/cep/ConsultaViaCepApiApplication.java
@@ -0,0 +1,13 @@
+package com.militao.cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaViaCepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaViaCepApiApplication.class, args);
+	}
+
+}
